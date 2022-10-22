b7541399684b9f7f26d691ef21259b5bc0ff3474
nathangauth88 <nathangauth@gmail.com>
2022-10-21 21:30:19 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+// const triggerTabList = document.querySelectorAll('#myTab button')
+// triggerTabList.forEach(triggerEl => {
+//   const tabTrigger = new bootstrap.Tab(triggerEl)
+
+//   triggerEl.addEventListener('click', event => {
+//     event.preventDefault()
+//     tabTrigger.show()
+//   })
+// })
+
+// const triggerTabList = document.querySelectorAll('#myTab button')
+// triggerTabList.forEach(triggerEl => {
+//   const tabTrigger = new bootstrap.Tab(triggerEl)
+
+//   triggerEl.addEventListener('click', event => {
+//     event.preventDefault()
+//     tabTrigger.show()
+//   })
+// })
